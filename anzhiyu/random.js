25dd36dcf31d6c2a8b3b039e1f0c67ab33a1927c
MartinLiuMingZhi <>
2025-09-29 02:46:08 +0000

    Publish Post source/_posts/Ktor 学习与实践文档.md by Qexo UTC 2025-09-29 Monday 02:46:08 Updated By Github Actions

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["2024/07/29/Android-EventBus/","2024/06/18/Android三种动画学习/","2024/05/21/Android面经/","2025/09/21/Flutter - AutoRoute/","2024/06/10/Flutter/","2024/06/17/Glide - 一个好用的图片加载库/","2024/07/03/HarmonyOS-ArkTS语法/","2025/01/20/HarmonyOS-两种PreferencesUtil实现方式/","2024/05/21/Java面经/","2024/03/19/Kotlin常见面试知识点/","2025/09/28/Ktor 学习与实践文档/","2024/05/26/RecyclerView底层实现原理/","2024/09/09/RxJava 面试题/","2024/07/25/RxJava-函数响应式编程/","2024/03/18/hello-world/","2024/06/10/主题配置/","2024/06/10/图床/","2024/05/18/安卓面试重点/","2024/06/10/编译原理名词解释/","2024/03/19/网络编程面试题/","2024/03/20/字节一面面经/","2024/06/18/设计模式/","2024/05/20/阅文面试/","2024/07/21/高新兴实习第一周/","2024/08/11/高新兴实习第三周/","2024/08/11/高新兴实习第二周/","2024/12/01/HarmonyOS-@ohos/axios网络请求/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
